e5e8fdc89a6897fa5e1b8610b56324332581a895
XShadowWizardX <68756969+XShadowWizardX@users.noreply.github.com>
2021-05-26 20:41:17 -0500

    Update index.js

--- index.js
+++ index.js
@@ -12,13 +12,14 @@ module.exports = {
             UserProfile: require("./source/foundation/UserProfile"),
             UserRequest: require("./source/foundation/UserRequest"),
             woofPack: require("./source/foundation/woofPack"),
-            
+            woofPackLevel: require("./source/foundation/woofPackLevel")
         },
 
         managers: {
             BaseManagerArray: require("./source/managers/BaseManagerArray"),
             BaseManagerCollection: require("./source/managers/BaseManagerCollection"),
             
+            woofPackLevelsManager: require("./source/managers/woofPackLevelsManager")
         },
 
         properties: {
@@ -31,12 +32,12 @@ module.exports = {
         },
 
         util: {
-            
-            
-            
-            
-            
-            
+            Collection: require("./source/util/Collection"),
+            Color: require("./source/util/Color"),
+            Constants: require("./source/util/Constants"),
+            Emoji: require("./source/util/Emoji"),
+            EncodeNumberToCharString: require("./source/util/EncodeNumberToCharString"),
+            PList: require("./source/util/PList")
         }
 
     }

de32df3633c4440a69354983c078032db3f11444
XShadowWizardX <68756969+XShadowWizardX@users.noreply.github.com>
2021-06-20 23:04:55 -0500

    Nice and pretty

--- client/actions/Action.js
+++ client/actions/Action.js
@@ -19,13 +19,13 @@ class Action {
 
     getHandlerType(type=undefined) {
         return type && type in HANDLES_EVENTS
-        ? HANDLES_EVENTS[type]
+            ? HANDLES_EVENTS[type]
         : this.constructor.name.startsWith(HANDLES_EVENTS.actionSend)
-        ? HANDLES_EVENTS.actionSent
+            ? HANDLES_EVENTS.actionSent
         : this.constructor.name.startsWith(HANDLES_EVENTS.actionCreate)
-        ? HANDLES_EVENTS.actionCreated
+            ? HANDLES_EVENTS.actionCreated
         : this.constructor.name.startsWith(HANDLES_EVENTS.actionGet)
-        ? HANDLES_EVENTS.actionGet
+            ? HANDLES_EVENTS.actionGet
         : HANDLES_EVENTS.action;
     }
 

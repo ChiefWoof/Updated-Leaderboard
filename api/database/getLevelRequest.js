03b26260675e446058722261c56dc7df933fc16a
XShadowWizardX <68756969+XShadowWizardX@users.noreply.github.com>
2021-06-27 21:46:26 -0500

    "getLevelRequest" endpoint 👀

--- api/database/getLevelRequest.js
+++ api/database/getLevelRequest.js
@@ -1,23 +1,18 @@
 "use strict";
 
-const http = require("http");
-const Base = require("./Base");
+const Base = require("./BaseLevelRequests");
 
 const { API_CODES } = require("../../source/util/Constants");
-const PROPERTY_LIST = require("../../source/properties/endpoints").getLevelScore;
-const fs = require("fs");
-const path = require("path");
-
-const LevelScore = require("../../source/foundation/LevelScore");
+const PROPERTY_LIST = require("../../source/properties/endpoints").getLevelRequest;
 
 class getLevelRequest extends Base {
 
-    static DIRECTORY = `${this.DIRECTORY}/${this.name}`;
     static SUPPORTED = true;
     static OFFLINE = false;
 
     static PROPERTY_LIST = PROPERTY_LIST;
     static PROPERTIES_LOADED = -1;
+    static SETS = {};
 
     constructor(data, client) {
         super(data, client);
@@ -25,51 +20,20 @@ class getLevelRequest extends Base {
     }
 
     /**
-     * @returns {boolean} Whether the current parameters will clearly produce a faulty return
+     * @todo add a check for negative level IDs and certifying they're XShadowWizardX
+     * @returns {boolean} Whether the current parameters indicate the user passes permission requirements
+     * @override
      */
 
-    isFaulty() {
-        return ![
-            this.levelID > 0,
-            this.levelID < 0
-        ].includes(true);
+    hasPermission() { return super.hasPermission(); }
+
+    async handlerAction() {
+        if (await this.entryExists()) {
+            let d = await this.getEntryAsLevelRequest();
+            if (d.isUseable()) return d;
+        }
+        return API_CODES.NO_DATA;
     }
-
-    get directoryScores() { return path.resolve(__dirname, "../../source/database/levelScores"); }
-
-    async handler() {
-        return await super.handler(async () => {
-            let files = await fs.readdirSync(this.directoryScores);
-            if (files.includes(`${this.levelID}.json`)) {
-                let d = await fs.readFileSync(`${this.directoryScores}/${this.levelID}.json`).toString() || '""';
-                d = new LevelScore(JSON.parse(d));
-                if (d.isUseable()) return this.json ? d : d.stringify();
-            }
-            return API_CODES.NO_DATA;
-        });
-    }
-
-    build(data) {
-        data = this.parse(data);
-        super.build(data);
-
-        /**
-         * @description The Level ID
-         * @default 0n
-         * @type {BigInt}
-         */
-
-        this.levelID = "levelID" in data ? data.levelID : 0n;
-        
-        return this;
-    }
-
-    /**
-     * @default 0n
-     * @param {?number|string|BigInt} [value=0n]
-     */
-
-    setLevelID(value=0n) { return this; }
 
 }
 

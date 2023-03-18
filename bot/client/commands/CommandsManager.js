6f20296f5e6a94c02125cee98bc455129aacb708
ChiefWoof <68756969+ChiefWoof@users.noreply.github.com>
2023-03-18 10:43:51 -0500

    Update CommandsManager.js

--- bot/client/commands/CommandsManager.js
+++ bot/client/commands/CommandsManager.js
@@ -1,11 +1,13 @@
 "use strict";
 
+const addUserCommand = require("./addUser");
 const leaderboardWhitelistDownloadCommand = require("./leaderboardWhitelistDownload");
 const usersULDownloadCommand = require("./usersULDownload");
 const leaderboardCommand = require("./leaderboard");
 const pingCommand = require("./ping");
 const profileCommand = require("./profile");
 const progressCommand = require("./progress");
+const requestUserCommand = require("./requestUser");
 const timeCommand = require("./time");
 const statProgressDownloadCommand = require("./statProgressDownload");
 
@@ -13,17 +15,20 @@ class ActionsManager {
 
     constructor(client) {
 
+        /** @type {addUserCommand} */
+        this.addUser = new addUserCommand(client);
+        this.addUser.useable = true;
+        this.addUser.beta = true;
+
         /** @type {leaderboardWhitelistDownloadCommand} */
         this.leaderboardWhitelistDownload = new leaderboardWhitelistDownloadCommand(client);
         this.leaderboardWhitelistDownload.useable = true;
         this.leaderboardWhitelistDownload.beta = true;
-        this.leaderboardWhitelistDownload.sg = true;
 
         /** @type {usersULDownloadCommand} */
         this.usersULDownload = new usersULDownloadCommand(client);
         this.usersULDownload.useable = true;
         this.usersULDownload.beta = true;
-        this.usersULDownload.sg = true;
 
         /** @type {pingCommand} */
         this.ping = new pingCommand(client);
@@ -39,6 +44,12 @@ class ActionsManager {
         this.progress = new progressCommand(client);
         this.progress.useable = true;
         this.progress.beta = true;
+
+        /** @type {requestUserCommand} */
+        this.requestUser = new requestUserCommand(client);
+        this.requestUser.useable = true;
+        this.requestUser.beta = true;
+        this.requestUser.betaOnly = true;
 
         /** @type {statProgressDownloadCommand} */
         this.statProgressDownload = new statProgressDownloadCommand(client);

d2f58b43a947f1645325404a4f8610ab0f65ca3e
ChiefWoof <68756969+ChiefWoof@users.noreply.github.com>
2024-01-22 00:46:26 -0600

    Update Province.js

--- areas/src/Province.js
+++ areas/src/Province.js
@@ -1,7 +1,7 @@
 "use strict";
 
 const BaseTerritory = require("./BaseTerritory");
-const Util = require("../util/Util");
+const Util = require("../../src/util/Util");
 const provinces = require("../storage/provinces.json");
 
 /**
